4ce01b7594eb64232b537968b4a640e4a81d4a16
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2023-06-17 02:15:45 -0400

    Se intendo ponerle animacion
    cada vez mas bug, mejor sin animación

--- JS/Index.js
+++ JS/Index.js
@@ -8,3 +8,42 @@ function AbrirElMenu() {
     document.getElementById("CerrarFuera").style.width = "0%";
     document.getElementById("CerrarFuera").style.transitionDelay = "0s";
   }
+
+
+
+
+  let OrdenMostrar = 1;
+  MostrarVisibles(OrdenMostrar);
+  
+  function Mover(n) {
+    document.getElementsByClassName("DestacadosSeccion")[0].style.flexDirection = "row";
+    MostrarVisibles(OrdenMostrar += n);    
+  }
+  
+
+  
+  function MostrarVisibles(n) {
+    let i;
+    let Disponibles = document.getElementsByClassName("juego");
+
+    if (n > Disponibles.length) {
+      OrdenMostrar = 1;
+    }
+    if (n < 1) {
+      OrdenMostrar = Disponibles.length;
+    }
+
+    for (i = 0; i < Disponibles.length; i++) {
+      Disponibles[i].style.display = "none";
+    }
+  
+    Disponibles[OrdenMostrar-1].style.display = "block";
+    if (Disponibles[OrdenMostrar]) {
+      Disponibles[OrdenMostrar].style.display = "block";
+    }
+    if (!Disponibles[OrdenMostrar]) {
+      Disponibles[0].style.display = "block";
+      document.getElementsByClassName("DestacadosSeccion")[0].style.flexDirection = "row-reverse";
+      
+    }
+  }
